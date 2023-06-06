b019b569d9267375b2b0e08bc0c75fe62e267a30
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-06-05 20:12:42 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = var.aws_region
+}
+variable "s3_bucket_names" {
+  type = list
+  default = ["dev-bucket.app", "uat-bucket.app", "prod-bucket.app"]
+}
+resource "aws_s3_bucket" "rugged_buckets" {
+  count         = length(var.s3_bucket_names) 
+  bucket        = var.s3_bucket_names[count.index]
+  acl           = "private"
+  region        = "us-east-2"
+  force_destroy = true
+}
